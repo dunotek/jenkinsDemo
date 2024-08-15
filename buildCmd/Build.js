46c68ad217bb2f898539b72ed1d484488b294d73
duno <hue.nguyen@enotion.io>
2024-08-15 16:24:20 +0700

    setup build jenkins

--- buildCmd/Build.js
+++ buildCmd/Build.js
@@ -1,19 +1,22 @@
-const { exec } = require('child_process');
+const fs = require('fs-extra')
+const { exec, execSync } = require('child_process');
 const config = require("./config")
 const env = process.env;
 const platform = 'web-mobile'; // Nền tảng bạn muốn build, ví dụ: 'web-mobile', 'android', 'ios'
 const outputPath = '../build'; // Thư mục output
-
+const buildConfig = ' --project ../ --build "platform=web-desktop;debug=true"';
 // const command = `CocosCreator --path . --build "platform=${platform};buildPath=${outputPath}"`;
 
 console.log("Compiling project...");
-let buildCmd = `${config.COCOS_PATH} --nologin --project ../ --build "platform=${platform};buildPath=${outputPath}"`;
-// let buildCmd = `echo Heelo`;
-exec(buildCmd, (err, stdout, stderr) => {
-    if (err) {
-        console.log("err: " + err);
-        console.error(`Error: ${stderr}`);
-        return;
-    }
-    console.log(`Build successful:\n${stdout}`);
-});
+// let buildCmd = `${config.COCOS_PATH} --nologin --project ../ --build "platform=${platform};buildPath=${outputPath}"`;
+// let buildCmd = `echo Hello`;
+let buildCmd = config.COCOS_PATH + buildConfig + `;buildPath=${outputPath}`;
+execSync(buildCmd, {stdio: 'inherit'});
+// exec(buildCmd, (err, stdout, stderr) => {
+//     if (err) {
+//         console.log("err: " + err);
+//         console.error(`Error: ${stderr}`);
+//         return;
+//     }
+//     console.log(`Build successful:\n${stdout}`);
+// });
--- buildCmd/config.js
+++ buildCmd/config.js
@@ -3,12 +3,12 @@ const config =
 {
     COCOS_VERSION: "3.7.3",
     COCOS_PATH: '/Applications/CocosCreator/Creator/3.7.3/CocosCreator.app/Contents/MacOS/CocosCreator',
-    REPO_PATH: "/Users/jenkins/Repo373/",
+    // REPO_PATH: "/Users/jenkins/Repo373/",
     USE_CUSTOM_ENGINE: false,
-    IFRAME_USE_CUSTOM_ENGINE: true,
-	CUSTOM_ENGINE_PATH: '/Users/jenkins/DevTools/cc30-cocos-engine-v373/',
-    CUSTOM_ENGINE_BRANCH: 'v373',
-    MOCK_PATH: '/Users/jenkins/DevTools/mock_iframe3',
+    // IFRAME_USE_CUSTOM_ENGINE: true,
+	// CUSTOM_ENGINE_PATH: '/Users/jenkins/DevTools/cc30-cocos-engine-v373/',
+    // CUSTOM_ENGINE_BRANCH: 'v373',
+    // MOCK_PATH: '/Users/jenkins/DevTools/mock_iframe3',
     PROJECT_BRANCH: 'v373'
 }
 
