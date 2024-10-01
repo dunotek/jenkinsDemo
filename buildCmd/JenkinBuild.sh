b6edb8fff2e12723067136c9c9cea0338dee800f
duno <hue.nguyen@enotion.io>
2024-10-01 16:25:27 +0700

    test build

--- buildCmd/JenkinBuild.sh
+++ buildCmd/JenkinBuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt update -y
+sudo apt install curl -y
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
 npm -v
 node buildCmd/Build.js
